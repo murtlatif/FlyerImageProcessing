f4b144eaa693889acbe2c4eb51260358e6434725
murtlatif <taaz.murr@gmail.com>
2021-11-15 07:13:12 -0500

    fix ocr_bound_img

--- ocr_bound_img.py
+++ ocr_bound_img.py
@@ -6,19 +6,22 @@ from ocr.display_bounds import draw_text_annotations
 from ocr.recognize import get_text_annotations
 
 
-def draw_box_around_sign():
+def draw_boxes_around_text():
     image_path = Config.args.img
+    image_file_name = os.path.basename(image_path)
+    image_file_without_ext, _ = os.path.splitext(image_file_name)
+
     annotation_json_path = Config.args.annotations
+
     if annotation_json_path:
         response = ocr.google_cloud.image_response.load_response_from_json(annotation_json_path)
         annotations = ocr.google_cloud.image_response.response_to_ocr_annotations(response)
     else:
-        image_file_name = os.path.basename(image_path)
-        image_file_without_ext, _ = os.path.splitext(image_file_name)
         annotations = get_text_annotations(image_path, save_file=image_file_without_ext)
 
-    draw_text_annotations(image_path, annotations=annotations)
+    bounded_image = draw_text_annotations(image_path, annotations=annotations)
+    bounded_image.save(f'bounded_{image_file_without_ext}.png')
 
 
 if __name__ == '__main__':
-    draw_box_around_sign()
+    draw_boxes_around_text()
