d8ac7b1ce346e4a0e341295399acc0c2ed3944be
murtlatif <taaz.murr@gmail.com>
2021-11-29 00:56:45 -0500

    Add flyer/page level features in pipeline

--- util/file_path_util.py
+++ util/file_path_util.py
@@ -2,7 +2,7 @@ import os.path
 import pathlib
 
 
-def apply_default_file_ext(file_path: str, default_file_ext: str, force: bool = False):
+def apply_default_file_ext(file_path: str, default_file_ext: str, force: bool = False) -> str:
     """
     Appends the given default_file_ext to the file if it does not have
     a file extension already.
@@ -25,7 +25,7 @@ def apply_default_file_ext(file_path: str, default_file_ext: str, force: bool = 
     return file_path
 
 
-def get_file_name_without_ext(file_path: str):
+def get_file_name_without_ext(file_path: str) -> str:
     """
     Returns the base file name without the extension.
 
@@ -38,7 +38,7 @@ def get_file_name_without_ext(file_path: str):
     return file_name_without_ext
 
 
-def create_directories_to_file(file_path: str):
+def create_directories_to_file(file_path: str) -> None:
     """
     Creates directories up to the given file path.
     """
@@ -47,9 +47,29 @@ def create_directories_to_file(file_path: str):
         pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
 
 
-def has_extension(file_path: str, valid_extensions: set[str]):
+def has_extension(file_path: str, valid_extensions: set[str]) -> bool:
     """
     Checks if the file path contains an extension in the valid_extensions set.
     """
     file_extension = os.path.splitext(file_path)[1]
     return file_extension in valid_extensions
+
+
+def get_path_directory(file_path: str) -> str:
+    """
+    Gets the directory up to the file path, or returns the file path itself
+    if it is a directory.
+    """
+    file_path_directory = file_path if os.path.isdir(file_path) else os.path.dirname(file_path)
+    return file_path_directory
+
+
+def get_last_folder_in_path(file_path: str) -> str:
+    """
+    Gets the last directory in a file path.
+    """
+    directory = get_path_directory(file_path)
+    normalized_directory = os.path.normpath(directory)
+
+    last_folder = os.path.basename(normalized_directory)
+    return last_folder
